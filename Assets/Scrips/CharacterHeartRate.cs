17e56179e568490f665ab1ab7168036cd6ae48da
ThummanoonP <157278930+ThummanoonP@users.noreply.github.com>
2024-11-25 14:43:24 +0700

    Add Environment

--- Assets/Scrips/CharacterHeartRate.cs
+++ Assets/Scrips/CharacterHeartRate.cs
@@ -35,7 +35,7 @@ public class CharacterHeartRate : MonoBehaviour
     {
         if (CPR.activeSelf) 
         {
-            PumpRate++;
+            //PumpRate++;
             HeartRateUp();
         }
     }
